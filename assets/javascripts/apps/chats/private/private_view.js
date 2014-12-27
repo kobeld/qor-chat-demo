265ae5f794a22c2a7ec91a7c99b0ce4c42fbc1db
kobeld <kobeld@163.com>
2014-12-27 16:41:01 +0800

    Add online indicator to user detail view

--- assets/javascripts/apps/chats/private/private_view.js
+++ assets/javascripts/apps/chats/private/private_view.js
@@ -1,8 +1,33 @@
 App.module("ChatsApp.Private", function (Private, App, Backbone, Marionette, $, _) {
 
+	// TODO:
+	// Think about how to abstract the OnlineIndicator from UserInfoView and RosterItemView
 	Private.UserInfoView = Marionette.ItemView.extend({
 		className: "sidebar-scroll",
-		template: "#user-info-sidebar-view"
+		template: "#user-info-sidebar-view",
+
+		ui: {
+			onlineIndicator: ".online-indicator"
+		},
+
+		templateHelpers: function () {
+			return {
+				onlineIcon: this.model.get("isOnline") ? "text-success" : "text-muted",
+			};
+		},
+
+		modelEvents: {
+			"change:isOnline": "updateOnlineIndicator"
+		},
+
+		updateOnlineIndicator: function () {
+			if (this.model.get("isOnline")) {
+				this.ui.onlineIndicator.removeClass("text-muted").addClass("text-success");
+			} else {
+				this.ui.onlineIndicator.removeClass("text-success").addClass("text-muted");
+			};
+		}
+
 	});
 
 });
